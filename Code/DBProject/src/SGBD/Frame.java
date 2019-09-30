609b7848c7820767206874ab3caf45527caac5f2
tlerou <47148358+tlerou@users.noreply.github.com>
2019-09-30 16:27:09 +0200

    Update Frame.java

--- Code/DBProject/src/SGBD/Frame.java
+++ Code/DBProject/src/SGBD/Frame.java
@@ -3,12 +3,12 @@ package SGBD;
 import java.nio.ByteBuffer;
 
 public class Frame {
-	private ByteBuffer buffer;
+	private byte[] buffer;
 	private PageId pageId;
 	private int pin_count;
 	private boolean dirty;
 	
-	public Frame(ByteBuffer buffer,PageId pageId,int pin_count,boolean dirty) {
+	public Frame(byte[] buffer,PageId pageId,int pin_count,boolean dirty) {
 		this.buffer=buffer;
 		this.pageId=pageId;
 		this.pin_count=pin_count;
