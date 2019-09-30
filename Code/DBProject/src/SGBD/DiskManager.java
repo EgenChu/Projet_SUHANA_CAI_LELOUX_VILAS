0936f1fe806d26e22423e76d694d9c1e0c90f476
Olivier-Cai <55697235+Olivier-Cai@users.noreply.github.com>
2019-09-30 15:39:15 +0200

    Update DiskManager.java

--- Code/DBProject/src/SGBD/DiskManager.java
+++ Code/DBProject/src/SGBD/DiskManager.java
@@ -3,6 +3,7 @@ package SGBD;
 import java.io.RandomAccessFile;
 import java.nio.file.Path;
 import java.io.File;
+import java.nio.ByteBuffer;
 
 public class DiskManager {
 	public static void createFile(int fileIdx) throws Exception{
@@ -11,8 +12,24 @@ public class DiskManager {
 	}
 	
 	public static PageId addPage(int fileIdx) {
-//		Path path = ;
-		File f =  File("Projet_SUHANA_CAI_LELOUX_VILAS/DB/"+"Data_"+ fileIdx+".rf");
 		
+		File fichier = new File("Projet_SUHANA_CAI_LELOUX_VILAS/DB/"+"Data_"+ fileIdx+".rf");
+		RandomAccessFile f = new RandomAccessFile(fichier,"rw");
+		f.setLength(f.length()+ Constants.pageSize);
+		PageId p = new PageId(fileIdx,(f.length()/Constants.pageSize)-1);
+		return p;
+	}
+	
+	public static void readPage(PageId p,ByteBuffer buff) {
+		File fichier = new File("Projet_SUHANA_CAI_LELOUX_VILAS/DB/"+"Data_"+ p.fileIdx+".rf");
+		RandomAccessFile f = new RandomAccessFile(fichier,"rw");	
+		f.readFully(buff,f.seek(p.pageIdx*Constants.pageSize),Constants.pageSize);
+	}
+	
+	public static void writePage(PageId p,ByteBuffer buff) {
+		File fichier = new File("Projet_SUHANA_CAI_LELOUX_VILAS/DB/"+"Data_"+ p.fileIdx+".rf");
+		RandomAccessFile f = new RandomAccessFile(fichier,"rw");
+		f.write(buff,f.seek(p.pageIdx*Constants.pageSize),Constants.pageSize);
 	}
 }
+
