26297f1fc72abd7fc19dfabddbbdad9382ed6533
Egen <sutra.patria.suhana@gmail.com>
2019-10-07 14:23:26 +0200

    Ajout get set classe Frame

--- Code/DBProject/src/SGBD/Frame.java
+++ Code/DBProject/src/SGBD/Frame.java
@@ -1,19 +1,59 @@
 package SGBD;
 
 import java.nio.ByteBuffer;
+import java.sql.Timestamp;
 
 public class Frame {
 	private ByteBuffer buffer;
 	private PageId pageId;
 	private int pin_count;
 	private boolean dirty;
+	private Timestamp time;
 	
-	public Frame(PageId pageId,int pin_count,boolean dirty) {
+	public Frame(PageId pageId) {
 		ByteBuffer.allocate(Constants.PAGE_SIZE);
-		this.pageId=pageId;
-		this.pin_count=pin_count;
-		this.dirty=dirty;
+		this.setPageId(pageId);
+		this.setPin_count(0);
+		this.setDirty(false);
+		setTime(null);
 	}
+
+	public PageId getPageId() {
+		return pageId;
+	}
+
+	public void setPageId(PageId pageId) {
+		this.pageId = pageId;
+	}
+
+	public int getPin_count() {
+		return pin_count;
+	}
+
+	public void setPin_count(int pin_count) {
+		this.pin_count = pin_count;
+	}
+
+	public boolean isDirty() {
+		return dirty;
+	}
+
+	public void setDirty(boolean dirty) {
+		this.dirty = dirty;
+	}
+
+	public ByteBuffer getBuffer() {
+		return buffer;
+	}
+
+	public Timestamp getTime() {
+		return time;
+	}
+
+	public void setTime(Timestamp time) {
+		this.time = time;
+	}
+	
 	
 
 }
