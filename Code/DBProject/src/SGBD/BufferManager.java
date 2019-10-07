8e6a5587ee681110871d83a7e196de1eb84929c8
Tlerou <leroux.tibo@gmail.com>
2019-10-07 16:35:28 +0200

    GetPage de BufferManager fini

--- Code/DBProject/src/SGBD/BufferManager.java
+++ Code/DBProject/src/SGBD/BufferManager.java
@@ -1,42 +1,51 @@
 package SGBD;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
 public class BufferManager {
-	
-	Frame[] frames = new Frame[Constants.FRAME_COUNT] ;
-	
+
+	Frame[] frames = new Frame[Constants.FRAME_COUNT];
+
 	private BufferManager() {
-		
+
 	}
 
 	private static BufferManager bufferManager = null;
 
 	public static BufferManager getInstance() {
 		if (bufferManager == null)
-			bufferManager = new BufferManager ();
-		return bufferManager ;
+			bufferManager = new BufferManager();
+		return bufferManager;
 	}
-	
+
 	public ByteBuffer GetPage(PageId pageId) {
-		int espace;
-		for(int i = 0; i < frames.length; i++) {
-			if(frames[i].getPin_count()== 0) {
-				espace = i;
-				break;
+		int espace = Constants.FRAME_COUNT;
+		for (int i = 0; i < frames.length; i++) {
+			if (frames[i].getPin_count() == 0) {
+				if (!frames[i].isRefbit()) {
+					espace = i;
+					break;
+				}
 			}
 		}
-		for(int i = 0; i < frames.length; i++) {
-			if(!frames[i].isRefbit()) {
-				espace = i;
-				break;
+		if (espace == Constants.FRAME_COUNT) {
+			System.out.println("Erreur dans le code");
+			return null;
+		} else {
+			try {
+				DiskManager.getInstance().readPage(pageId, frames[espace].getBuffer());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+
+			return frames[espace].getBuffer();
+
 		}
-		
-		
 	}
-	
-	public void FreePage (PageId pageId, boolean valdirty) {
-		
+
+	public void FreePage(PageId pageId, boolean valdirty) {
+
 	}
 }
