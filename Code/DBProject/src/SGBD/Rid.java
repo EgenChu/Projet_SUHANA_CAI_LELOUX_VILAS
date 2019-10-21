88048fba8dc1610accf3c2cecf16ad55eb815d3a
Tlerou <leroux.tibo@gmail.com>
2019-10-21 16:46:09 +0200

    rid

--- Code/DBProject/src/SGBD/Rid.java
+++ Code/DBProject/src/SGBD/Rid.java
@@ -1,10 +1,10 @@
 package SGBD;
 
-public class RId {
+public class Rid {
 	private PageId pageId;
 	private int slotIdx;
 	
-	public RId(PageId pageId, int slotIdx) {
+	public Rid(PageId pageId, int slotIdx) {
 		this.pageId = pageId;
 		this.slotIdx = slotIdx;
 	}
