2cacf5c494fbcc236afefefc718b9641b79e5cd2
Olivier <caiolivier3001@gmail.com>
2019-10-21 14:15:18 +0200

    Ajout de la classe Rid

---
+++ Code/DBProject/src/SGBD/Rid.java
@@ -0,0 +1,19 @@
+import SGBD;
+
+public class Rid {
+	private PageId pageId;
+	private int slotIdx;
+	
+	public Rid(PageId pageId, int slotIdx) {
+		this.pageId = pageId;
+		this.slotIdx = slotIdx;
+	}
+	
+	public PageId getPageId() {
+		return pageId;
+	}
+	
+	public Int getSlotIdx() {
+		return slotIdx;
+	}
+}
