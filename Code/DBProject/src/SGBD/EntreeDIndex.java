4e0dea0f7562995870de4ad0cf7148e166f6026f
Tlerou <leroux.tibo@gmail.com>
2019-12-02 14:11:19 +0100

    b+tree ajout classes

---
+++ Code/DBProject/src/SGBD/EntreeDIndex.java
@@ -0,0 +1,11 @@
+package SGBD;
+
+public class EntreeDIndex {
+	private int cle;
+	private Noeud fils;
+	
+	public EntreeDIndex(int cle, Noeud fils) {
+		this.cle = cle;
+		this.fils = fils;
+	}
+}
---
+++ Code/DBProject/src/SGBD/EntreeDeDonnees.java
@@ -0,0 +1,14 @@
+package SGBD;
+
+import java.util.List;
+
+public class EntreeDeDonnees {
+	private int cle;
+	private List<Rid> rid;
+	
+	public EntreeDeDonnees(int cle ,List<Rid> rid) {
+		this.cle = cle;
+		this.rid =rid;	
+	}
+
+}
